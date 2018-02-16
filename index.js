471febd6e3647a3a9427d7d115e97b9c60dcce69
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-16 21:51:25 +1300

    Add basic coin function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const b64 = hex => new Buffer(hex.replace(/-/g,''), 'hex').toString('base64')
+
 const coinable = (die, planchet) => die.length <= 22
 
+const coin = (die, planchet) => die + b64(planchet).slice(die.length, planchet.length)
+
 module.exports = {
-  coinable
+  coin,
+  coinable,
 }
--- index.spec.js
+++ index.spec.js
@@ -22,3 +22,12 @@ test('coinable, die too long', t => {
   const actual = sut.coinable(die, planchet)
   t.false(actual)
 })
+
+test('coin', t => {
+  const planchet = 'c8404eda-83cb-4728-bf68-21bd1bac26b8'
+  // yEBO2oPLRyi/aCG9G6wmuA==
+  const die = 'jane+doe'
+  const actual = sut.coin(die, planchet)
+  const expected = 'jane+doeRyi/aCG9G6wmuA=='
+  t.is(actual, expected)
+})
