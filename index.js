511a0160bea3d4534fe979dafb568ab7f9900d8f
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-16 23:03:16 +1300

    Add coinAll function

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const R = require('ramda')
+const uv = require('uuid-validate')
+
 const b64 = hex => new Buffer(hex.replace(/-/g,''), 'hex').toString('base64')
+const b64ToHex = b64 => new Buffer(b64, 'base64').toString('hex')
+// RFC 4122 : 8-4-4-4-12
+const hexToRfc = hex => {
+  let x = 0
+  return hex.substring(x, x+=8)
+   +'-'+ hex.substring(x, x+=4)
+   +'-'+ hex.substring(x, x+=4)
+   +'-'+ hex.substring(x, x+=4)
+   +'-'+ hex.substring(x)
+}
 
 const coinable = (die, planchet) => die.length <= 22
 
 const coin = (die, planchet) => die + b64(planchet).slice(die.length, planchet.length)
 
+const coinAll = (die, planchet) => {
+  const planchet64 = b64(planchet)
+  const candidates = R.map(
+    i => planchet64.slice(0, i) + die + planchet64.slice(i+die.length),
+    R.range(0, 23 - die.length)
+  )
+  return R.filter(
+    R.pipe(b64ToHex, hexToRfc, uv),
+    candidates
+  )
+}
+
 module.exports = {
   coin,
+  coinAll,
   coinable,
 }
--- index.spec.js
+++ index.spec.js
@@ -31,3 +31,30 @@ test('coin', t => {
   const expected = 'jane+doeRyi/aCG9G6wmuA=='
   t.is(actual, expected)
 })
+
+test('coinAll', t => {
+  const planchet = 'c8404eda-83cb-4728-bf68-21bd1bac26b8'
+  // yEBO2oPLRyi/aCG9G6wmuA==
+  const die = 'jane+d'
+  const actual = sut.coinAll(die, planchet)
+  const expected = [
+    'jane+dPLRyi/aCG9G6wmuA==',
+    'yjane+dLRyi/aCG9G6wmuA==',
+    'yEjane+dRyi/aCG9G6wmuA==',
+    // 'yEBjane+dyi/aCG9G6wmuA==',
+    // 'yEBOjane+di/aCG9G6wmuA==',
+    // 'yEBO2jane+d/aCG9G6wmuA==',
+    // 'yEBO2ojane+daCG9G6wmuA==',
+    // 'yEBO2oPjane+dCG9G6wmuA==',
+    // 'yEBO2oPLjane+dG9G6wmuA==',
+    'yEBO2oPLRjane+d9G6wmuA==',
+    //'yEBO2oPLRyjane+dG6wmuA==',
+    'yEBO2oPLRyijane+d6wmuA==',
+    'yEBO2oPLRyi/jane+dwmuA==',
+    'yEBO2oPLRyi/ajane+dmuA==',
+    'yEBO2oPLRyi/aCjane+duA==',
+    'yEBO2oPLRyi/aCGjane+dA==',
+    'yEBO2oPLRyi/aCG9jane+d==',
+  ]
+  t.deepEqual(actual, expected)
+})
