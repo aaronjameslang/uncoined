fe9b08377506e28a005ce931523c777c4b3bcd83
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-20 12:35:49 +1300

    Add more uuids

--- decode.spec.js
+++ decode.spec.js
@@ -2,10 +2,16 @@ import test from 'ava'
 
 import decode from './decode'
 
-const uuid4 = 'c8404eda-83cb-4728-bf68-21bd1bac26b8'
+const uuidNil = '00000000-0000-0000-0000-000000000000'
 
-test('decode uuid4', t => {
-  const actual = decode.fromRfc(uuid4)
+const uuidV11A = '70008d18-15cc-11e8-b642-0ed5f89f718b'
+const uuidV11B = '7000933a-15cc-11e8-b642-0ed5f89f718b'
+const uuidV11C = '700094d4-15cc-11e8-b642-0ed5f89f718b'
+
+const uuidV41 = 'c8404eda-83cb-4728-bf68-21bd1bac26b8'
+
+test('decode uuidV41', t => {
+  const actual = decode.fromRfc(uuidV41)
   const expected = {
     variant: 1,
     version: 4
