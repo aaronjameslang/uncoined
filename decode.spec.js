ab5dbe4ce6610cd2dcc9a24057d9bf1488b17ffa
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-20 19:36:23 +1300

    Update tests for decode

--- decode.spec.js
+++ decode.spec.js
@@ -1,18 +1,26 @@
-import test from 'ava'
+const pathJoin = require('path').join
+const decode = require('./decode')
+const snapshot = require('snap-shot')
+const test = require('ava')
 
-import decode from './decode'
+const snapfile = pathJoin(
+  __dirname,
+  '__snapshots__',
+  __filename.slice(__dirname.length + 1) + '.snap-shot'
+)
+const snaps = require(snapfile)
 
-const uuidNil = '00000000-0000-0000-0000-000000000000'
+const testSnap = snap => {
+  test('decode ' + snap.rfc, t => {
+    t.plan(0)
+    const decoded = decode.fromRfc(snap.rfc)
+    snapshot({
+      rfc: snap.rfc,
+      decoded
+    })
+  })
+}
 
-const uuidV11A = '70008d18-15cc-11e8-b642-0ed5f89f718b'
-const uuidV11B = '7000933a-15cc-11e8-b642-0ed5f89f718b'
-const uuidV11C = '700094d4-15cc-11e8-b642-0ed5f89f718b'
-
-const uuidV41A = 'c8404eda-83cb-4728-bf68-21bd1bac26b8'
-const uuidV41B = 'cb9a9e10-d511-43d6-96b6-20738b96f783'
-const uuidV41C = '70d490cb-6269-4337-8d2e-96be6c6701ff'
-
-test('decode uuidV41', t => {
-  const actual = decode.fromRfc(uuidV41A)
-  t.snapshot(actual)
-})
+for (const snap in snaps) {
+  testSnap(snaps[snap])
+}
