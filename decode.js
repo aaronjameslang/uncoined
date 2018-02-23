41faf936b9ad11e6ded1ebbbd9923da55ecb5873
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-02-23 22:17:00 +1300

    Arrange and document

--- decode.js
+++ decode.js
@@ -1,6 +1,22 @@
 const libtime = require('./timestamp')
 
-// S 4.1.2 Fields
+/**
+ * Reading and writing UUID fields according to RFC 4122
+ * {@link https://tools.ietf.org/html/rfc4122}
+ */
+
+/**
+ * [§ 4.1.1 Variant]{@link https://tools.ietf.org/html/rfc4122#section-4.1.1}
+ */
+function readVariant (uuid) {
+  const octit = uuid.readUInt8(8) >> 5
+  return [0, 0, 0, 0, 1, 1, 2, 3][octit]
+}
+
+/**
+ * [§ 4.1.2 Layout and Byte Order]
+ *   {@link https://tools.ietf.org/html/rfc4122#section-4.1.2}
+ */
 
 function readTimeLow (uuid) {
   return uuid.readUInt32BE(0)
@@ -22,19 +38,17 @@ function readNode (uuid) {
   return uuid.readUIntBE(10, 6)
 }
 
-// S 4.1.1
-function readVariant (uuid) {
-  const octit = uuid.readUInt8(8) >> 5
-  return [0, 0, 0, 0, 1, 1, 2, 3][octit]
-}
-
-// S 4.1.3
+/**
+ * [§ 4.1.3 Version]{@link https://tools.ietf.org/html/rfc4122#section-4.1.3}
+ */
 function readVersion (uuid) {
   return uuid.readUInt8(6) >> 4
 }
 
-// S 4.1.4
-function readDateTime (uuid) {
+/**
+ * [§ 4.1.4 Timestamp]{@link https://tools.ietf.org/html/rfc4122#section-4.1.4}
+ */
+function readTimestamp (uuid) {
   const unixTime = libtime.toUnixTime(
     readTimeHighAndVersion(uuid) & 0x0FFF,
     readTimeMid(uuid),
@@ -46,17 +60,24 @@ function readDateTime (uuid) {
   return date.toISOString().slice(0, -4) + nanoSeconds + 'Z'
 }
 
-// S 4.1.5
+/**
+ * [§ 4.1.5 Clock Sequence]{@link https://tools.ietf.org/html/rfc4122#section-4.1.5}
+ */
 function readClockSequence (uuid) {
   const variant = readVariant(uuid)
   const mask = [0x7fff, 0x3fff, 0x1ff, 0x1ff][variant]
   return readClockSequenceAndReserved(uuid) & mask
 }
 
-// S 4.1.6
+/**
+ * [§ 4.1.6 Node]{@link https://tools.ietf.org/html/rfc4122#section-4.1.6}
+ * @see readNode
+ */
 function readMacAddress (uuid) {
   return readNode(uuid).toString(16).match(/.{2}/g).join(':')
 }
+
+// These need to be factored out
 
 function decode (uuid /* buffer */) {
   // TODO type check uuid
@@ -72,7 +93,7 @@ function decode (uuid /* buffer */) {
   decoded.version = readVersion(uuid)
 
   if (decoded.version === 1) {
-    decoded.dateTime = readDateTime(uuid)
+    decoded.timestamp = readTimestamp(uuid)
     decoded.clockSequnce = readClockSequence(uuid)
     decoded.macAddress = readMacAddress(uuid)
   }
