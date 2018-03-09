d0ee925830a298d3873c4677aabd922d6359b34c
Aaron J. Lang <aaronjameslang@googlemail.com>
2018-03-09 21:03:06 +1300

    Combine seconds and nanoseconds

--- src/lens/time.js
+++ src/lens/time.js
@@ -4,6 +4,57 @@
  */
 
 const bigInt = require('big-integer') // TODO remove this dependancy
+const { getTicks, setTicks } = require('./ticks')
+
+const NANOSECONDS_PER_TICK = 100
+const TICKS_PER_SECOND = Math.pow(10, 7)
+
+/**
+ * @param {string} uuid
+ * @returns {number} Unsigned integer < 10^9
+ * @example getNanoseconds('00000000-0000-1000-b467-0ed5f89f718b') // => 0
+ * @example getNanoseconds('00000001-0000-1000-b467-0ed5f89f718b') // => 100
+ * @example
+ * getNanoseconds('0000000a-0000-1000-b467-0ed5f89f718b') // => 1000
+ * @example
+ * getNanoseconds('0000270f-0000-1000-b467-0ed5f89f718b') // => 999900
+ * @example
+ * getNanoseconds('00002710-0000-1000-b467-0ed5f89f718b') // => 1000000
+ * @example
+ * getNanoseconds('0098967f-0000-1000-b467-0ed5f89f718b') // => 999999900
+ * @example
+ * getNanoseconds('00989680-0000-1000-b467-0ed5f89f718b') // => 0
+ * @example
+ * getNanoseconds('00989681-0000-1000-b467-0ed5f89f718b') // => 100
+ * @example <caption>10 seconds</caption>
+ * getNanoseconds('05f5e100-0000-1000-b467-0ed5f89f718b') // => 0
+ * @example
+ * getNanoseconds('9d6316aa-2106-11e8-b467-0ed5f89f718b') // => 883729000
+ * @static
+ */
+function getNanoseconds (uuid) {
+  const timeHexStr = getTicks(uuid)
+  const ticksBigInt = bigInt(timeHexStr, 16)
+  const nanoseconds = ticksBigInt
+    .mod(TICKS_PER_SECOND)
+    .times(NANOSECONDS_PER_TICK)
+    .toJSNumber()
+  return nanoseconds
+}
+
+/*
+ * Ideas for non-bigInt getNanos
+ * a = 0x100000 % 1000
+ * 576
+ * b = 0x10000000 % 1000
+ * 456
+ * c = a
+ * 576
+ * c = (c * 16) % 1000
+ * 216
+ * c = (c * 16) % 1000
+ * 456 // b
+ */
 
 /**
  * @param {string} uuid
@@ -37,6 +88,57 @@ function getSeconds (uuid) {
   const bint = bigInt(hex, 16)
   const seconds = bint.divide(Math.pow(10, 7)).toJSNumber()
   return seconds
+}
+
+function getTime (uuid) {
+  return [getSeconds(uuid), getNanoseconds(uuid)]
+}
+
+function setTime (time, uuid) {
+  const ticksBigInt = bigInt(time[0]).times(TICKS_PER_SECOND)
+    .plus(Math.floor(time[1] / NANOSECONDS_PER_TICK))
+  const ticksHexStr = ticksBigInt.toString(16)
+  return setTicks(ticksHexStr, uuid)
+}
+
+/**
+ * @param {number} Unsigned integer < 10^9
+ * @param {string} uuid
+ * @returns {string} uuid
+ * @example
+ * setNanoseconds(0, '00000000-0000-1000-b467-0ed5f89f718b')
+ *             // => '00000000-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(1, '00000000-0000-1000-b467-0ed5f89f718b')
+ *             // => '00000000-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(99, '00000000-0000-1000-b467-0ed5f89f718b')
+ *              // => '00000000-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(100, '00000000-0000-1000-b467-0ed5f89f718b')
+ *               // => '00000001-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(1000, '00000000-0000-1000-b467-0ed5f89f718b')
+ *                // => '0000000a-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(999999900, '00000000-0000-1000-b467-0ed5f89f718b')
+ *                     // => '0098967f-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(999999999, '00000000-0000-1000-b467-0ed5f89f718b')
+ *                     // => '0098967f-0000-1000-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(883729000, '9d6316aa-2106-11e8-b467-0ed5f89f718b')
+ *                     // => '9d6316aa-2106-11e8-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(0, '9d6316aa-2106-11e8-b467-0ed5f89f718b')
+ *             // => '9cdc3e00-2106-11e8-b467-0ed5f89f718b'
+ * @example
+ * setNanoseconds(1300, '9d6316aa-2106-11e8-b467-0ed5f89f718b')
+ *                // => '9cdc3e0d-2106-11e8-b467-0ed5f89f718b'
+ * @static
+ */
+function setNanoseconds (nanoseconds, uuid) {
+  return setTime([getSeconds(uuid), nanoseconds], uuid)
 }
 
 // /**
@@ -86,6 +188,10 @@ function getSeconds (uuid) {
 // }
 
 module.exports = {
-  getSeconds
-  // setSeconds
+  getNanoseconds,
+  getSeconds,
+  getTime,
+  setNanoseconds,
+  // setSeconds,
+  setTime
 }
